f29791b16de65d37ed4af7869c221f8104421c4f
Diego Zambrano <diegozambrano2910@gmail.com>
2024-02-19 20:00:46 -0300

    testingServices

--- src/app/layouts/auth/auth.service.spec.ts
+++ src/app/layouts/auth/auth.service.spec.ts
@@ -1,46 +1,45 @@
-import { TestBed } from "@angular/core/testing";
-import { AuthService } from "./auth.service"
-import {
-    HttpClientTestingModule,
-    HttpTestingController,
-  } from '@angular/common/http/testing';
-import { Users } from "../dashboard/pages/users/models";
+import { TestBed } from '@angular/core/testing';
+import { AuthService } from './auth.service';
+import { Users } from '../dashboard/pages/users/models';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing'
 
-describe('Pruebas de AuthService',()=>{
-    let authService:AuthService;
-    let httpController:HttpTestingController
-    beforeEach(()=>{
+describe('Testing de authService', () => {
+    let authService: AuthService;
+    let httpController: HttpTestingController
+    beforeEach(() => {
         TestBed.configureTestingModule({
-            providers:[authService],
-            imports:[HttpClientTestingModule]
-        })
+            providers: [AuthService],
+            imports: [HttpClientTestingModule]
+        });
         authService = TestBed.inject(AuthService);
-        httpController=TestBed.inject(HttpTestingController)
+        httpController = TestBed.inject(HttpTestingController)
     })
 
-    it('AuthService debe estar definido',()=>{
+    it('AuthService debe estar definido', () => {
         expect(authService).toBeTruthy();
     })
 
-    it ('Al llamar login debe establecer un authUser',()=>{
-        const User : Users[]=[
+    it('Al llamar al login debe establecer authuser', () => {
+        const userFake: Users[] = [
             {
-                id : 1,
-                name: 'Juan',
-                lastname:'Peers',
-                email:'Jpeeers@gmail.com',
-                course:'frontend',
-                password:'1384s',
-                token:'5217'
+                id: 14747,
+                name: 'Sal',
+                lastname: 'Srein',
+                email: 'salrein@gmail.com',
+                course: 'front-end',
+                password: '8790',
+                token: 'sies52s',
             }
         ]
-
-        authService.login({email:'Jpeeers@gmail.com',password:'1384s'}).subscribe({
+        authService.login({email:'salrein@gmail.com',password:'8790'})
+        .subscribe({
             next:()=>{
-                expect(authService.authUser).toEqual(User[0])
+                expect(authService.authUser).toEqual(userFake[0]);
             }
-        })
-        httpController.expectOne({url:'http://localhost:3000/users?email=mock@mail.com&password=password',method:'get'}).flush(User);
-    })
-
-})
+        });
+        httpController.expectOne({
+            url:'http://localhost:3000/users?email=salrein@gmail.com&password=8790',
+            method:'get',
+        }).flush(userFake)
+    });
+});
